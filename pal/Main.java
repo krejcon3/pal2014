774839f32c255553f50c4ac8a312f25d49ebf213
Ondřej Krejčíř <krejcon3@gmail.com>
2014-10-16 09:27:58 +0200

    4/10

--- pal/Main.java
+++ pal/Main.java
@@ -1,16 +1,19 @@
 package pal;
 
 import java.io.*;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static int count = 0;
 	public static int limit = 0;
 	public static int[][] sites;
-	public static HashSet<Integer> unused;
+	public static boolean[] toUse;
 	public static int start;
 	public static double[] bestWays;
+
+	/**
+	 * zničit hashset -> nahradit za pole
+	 */
 
 	public static void main(String[] args) throws IOException {
 //		System.setIn(new FileInputStream("./src/test/pub01.in"));
@@ -18,52 +21,53 @@ public class Main {
 		bestWays = new double[count];
 		for (int i = 0; i < count; i++) {
 			start = i;
-			work((HashSet<Integer>) unused.clone(), new int[count], i, 0, 0);
-			unused.remove(i);
-			if (bestWays[unused.size()] > 0) {
-				System.out.println((int)Math.ceil(bestWays[unused.size()]));
+			toUse[i] = false;
+			work(toUse, new int[count], i, 0, 0);
+			toUse[i] = true;
+			if (bestWays[bestWays.length - i - 1] > 0) {
+				System.out.println((int)Math.ceil(bestWays[bestWays.length - i - 1]));
 				break;
 			}
 		}
 	}
 
-	public static void work(HashSet<Integer> array, int[] used, int site, double length, int depth) {
-		array.remove(site);
+	public static void work(boolean[] array, int[] used, int site, double length, int depth) {
 		used[depth] = site;
-//		printArray(used, depth + 1);
 		int tStart;
 		int tEnd;
 		boolean crossed;
-		if (!array.isEmpty()) {
-			for (int s : array) {
-//				System.out.print(site + " -> " + s);
-				crossed = false;
-				double toNext = getDistance(sites[site], sites[s]);
-				double toHome = getDistance(sites[s], sites[start]);
-				if (length + toNext + toHome > limit) {
-//					System.out.println(" " + (length + toNext) + " too long to return home (" + (length + toNext + toHome) + ")");
+		for (int index = 0; index < array.length; index++) {
+			if (!toUse[index]) {
+				continue;
+			}
+			crossed = false;
+			double toNext = getDistance(sites[site], sites[index]);
+			double toHome = getDistance(sites[index], sites[start]);
+			if (length + toNext + toHome > limit) {
+				continue;
+			}
+			if (bestWays[bestWays.length - start - 1] > 0 && length + toNext + toHome > bestWays[bestWays.length - start - 1]) {
+				continue;
+			}
+			tEnd = -1;
+			for (int i = 0; i < depth + 1; i++) {
+				tStart = tEnd;
+				tEnd = used[i];
+				if (tStart == -1) {
 					continue;
 				}
-//				System.out.println(" " + (length + toNext));
-				tEnd = -1;
-				for (int i = 0; i < depth + 1; i++) {
-					tStart = tEnd;
-					tEnd = used[i];
-					if (tStart == -1) {
-						continue;
-					}
-					if (sites[site][0] == sites[tEnd][0] && sites[site][1] == sites[tEnd][1]) {
-						continue;
-					}
-					if (intersectionFounded(sites[site], sites[s], sites[tStart], sites[tEnd])) {
-//						System.out.println("\tinterception founded: [" + site + "|" + s + "] vs [" + tStart + "|" + tEnd + "]");
-						crossed = true;
-						break;
-					}
+				if (sites[site][0] == sites[tEnd][0] && sites[site][1] == sites[tEnd][1]) {
+					continue;
 				}
-				if (!crossed) {
-					work((HashSet<Integer>)array.clone(), used.clone(), s, length + toNext, depth + 1);
+				if (intersectionFounded(sites[site], sites[index], sites[tStart], sites[tEnd])) {
+					crossed = true;
+					break;
 				}
+			}
+			if (!crossed && depth < count - 1) {
+				toUse[index] = false;
+				work(array, used, index, length + toNext, depth + 1);
+				toUse[index] = true;
 			}
 		}
 		tEnd = -1;
@@ -78,7 +82,6 @@ public class Main {
 				continue;
 			}
 			if (intersectionFounded(sites[site], sites[start], sites[tStart], sites[tEnd])) {
-//				System.out.println("\tinterception founded: [" + site + "|" + start + "] vs [" + tStart + "|" + tEnd + "]");
 				crossed = true;
 				break;
 			}
@@ -107,13 +110,13 @@ public class Main {
 			limit = Integer.parseInt(st.nextToken());
 		}
 		sites = new int[count][2];
-		unused = new HashSet<Integer>(2 * count);
+		toUse = new boolean[count];
 		int c = 0;
 		while ((line = br.readLine()) != null) {
 			st = new StringTokenizer(line);
 			sites[c][0] = Integer.parseInt(st.nextToken());
 			sites[c][1] = Integer.parseInt(st.nextToken());
-			unused.add(c++);
+			toUse[c++] = true;
 		}
 	}
 
@@ -128,7 +131,6 @@ public class Main {
 			x3 == x4 && y3 == y4){
 			return false;
 		}
-		// Fastest method, based on Franklin Antonio's "Faster Line Segment Intersection" topic "in Graphics Gems III" book (http://www.graphicsgems.org/)
 		double ax = x2-x1;
 		double ay = y2-y1;
 		double bx = x3-x4;
@@ -158,14 +160,9 @@ public class Main {
 			}
 		}
 		if (commonDenominator == 0){
-			// This code wasn't in Franklin Antonio's method. It was added by Keith Woodward.
-			// The lines are parallel.
-			// Check if they're collinear.
 			double y3LessY1 = y3-y1;
-			double collinearityTestForP3 = x1*(y2-y3) + x2*(y3LessY1) + x3*(y1-y2);   // see http://mathworld.wolfram.com/Collinear.html
-			// If p3 is collinear with p1 and p2 then p4 will also be collinear, since p1-p2 is parallel with p3-p4
+			double collinearityTestForP3 = x1*(y2-y3) + x2*(y3LessY1) + x3*(y1-y2);
 			if (collinearityTestForP3 == 0){
-				// The lines are collinear. Now check if they overlap.
 				if (x1 >= x3 && x1 <= x4 || x1 <= x3 && x1 >= x4 ||
 					x2 >= x3 && x2 <= x4 || x2 <= x3 && x2 >= x4 ||
 					x3 >= x1 && x3 <= x2 || x3 <= x1 && x3 >= x2){
