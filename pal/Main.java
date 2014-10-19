d49cfb193e75bba57a4f46088898a333626033fe
Ondřej Krejčíř <krejcon3@gmail.com>
2014-10-19 16:01:30 +0200

    metoda pro čtení

--- pal/Main.java
+++ pal/Main.java
@@ -4,13 +4,18 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("file.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        StringTokenizer st;
-        while ((line = br.readLine()) != null) {
-            st = new StringTokenizer(line);
-        }
+		read();
     }
+
+	public static void read() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		StringTokenizer st;
+		if ((line = br.readLine()) != null) {
+			st = new StringTokenizer(line);
+		}
+	}
 }
