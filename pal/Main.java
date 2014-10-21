1425431058db4ff94872ea23f33cf71af35eaae2
Ondřej Krejčíř <krejcon3@gmail.com>
2014-10-21 19:28:52 +0200

    HW2 10/10

--- pal/Main.java
+++ pal/Main.java
@@ -14,40 +14,6 @@ public class Main {
 	public static int longest = 0;
 
     public static void main(String[] args) throws IOException {
-//        System.setIn(new FileInputStream("./src/test/2/pub02.in"));
-		read();
-		Arrays.sort(edges, new Comparator<int[]>(){
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[2] == o2[2] ? 0 : o1[2] < o2[2] ? 1 : -1;
-			}
-		});
-		for (int[] edge : edges) {
-			int tempCost1 = costs[edge[0]];
-			int tempWay1 = ways[edge[0]];
-			int tempCost2 = costs[edge[1]];
-			int tempWay2 = ways[edge[1]];
-
-			if (tempWay2 + edge[2] > tempWay1 && (tempCost1 == 0 || tempCost1 > edge[2])) {
-				costs[edge[0]] = edge[2];
-				ways[edge[0]] = tempWay2 + edge[2];
-				if (ways[edge[0]] > longest) {
-					longest = ways[edge[0]];
-				}
-			}
-
-			if (tempWay1 + edge[2] > tempWay2 && (tempCost2 == 0 || tempCost2 > edge[2])) {
-				costs[edge[1]] = edge[2];
-				ways[edge[1]] = tempWay1 + edge[2];
-				if (ways[edge[1]] > longest) {
-					longest = ways[edge[1]];
-				}
-			}
-		}
-		System.out.println(longest);
-	}
-
-	public static void read() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		StringTokenizer st;
@@ -63,5 +29,47 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			edges[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
 		}
+		Arrays.sort(edges, new Comparator<int[]>(){
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[2] == o2[2] ? 0 : o1[2] < o2[2] ? 1 : -1;
+			}
+		});
+		for (int[] edge : edges) {
+			int tempCost1 = costs[edge[0]];
+			int tempWay1 = ways[edge[0]];
+			int tempCost2 = costs[edge[1]];
+			int tempWay2 = ways[edge[1]];
+
+			if (tempCost1 == 0 && tempCost2 == 0) {
+				costs[edge[0]] = edge[2];
+				costs[edge[1]] = edge[2];
+				ways[edge[0]] = edge[2];
+				ways[edge[1]] = edge[2];
+				longest = Math.max(longest, Math.max(ways[edge[0]], ways[edge[1]]));
+			} else {
+				if (edge[2] < tempCost2 && tempWay1 < tempWay2 + edge[2]) {
+					ways[edge[0]] = tempWay2 + edge[2];
+					costs[edge[0]] = edge[2];
+					longest = Math.max(longest, ways[edge[0]]);
+				}
+				if (edge[2] < tempCost1 && tempWay2 < tempWay1 + edge[2]) {
+					ways[edge[1]] = tempWay1 + edge[2];
+					costs[edge[1]] = edge[2];
+					longest = Math.max(longest, ways[edge[1]]);
+				}
+				if (edge[2] == tempCost2 && tempWay1 < edge[2]) {
+					ways[edge[0]] = edge[2];
+					costs[edge[0]] = edge[2];
+					longest = Math.max(longest, ways[edge[0]]);
+				}
+				if (edge[2] == tempCost1 && tempWay2 < edge[2]) {
+					ways[edge[1]] = edge[2];
+					costs[edge[1]] = edge[2];
+					longest = Math.max(longest, ways[edge[1]]);
+				}
+			}
+		}
+		System.out.println(longest);
 	}
 }
